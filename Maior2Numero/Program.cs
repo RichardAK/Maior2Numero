f081e047bac6c5120b19c6fe580768f8d30d371f
RichardAK <100002016+RichardAK@users.noreply.github.com>
2022-04-06 18:49:51 -0300

    Finalizado

---
+++ Maior2Numero/Program.cs
@@ -0,0 +1,17 @@
+﻿int n1, n2;
+
+Console.Write("Digite o primeiro número: "); 
+n1 = Convert.ToInt32(Console.ReadLine()); 
+
+Console.Write("Digite o segundo número: "); 
+n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 > n2)
+{   
+    Console.WriteLine($"{n1} é maior ");
+}
+
+else
+{   
+    Console.WriteLine($"{n2} é maior");
+}
